029d17d54c3bf1da88446ea1b4c01ef1b9e66af3
Mike Subelsky <mike@subelsky.com>
2017-05-22 14:45:20 -0400

    Fleshing out more jasmine setup, finished API client spec

--- spec/api-client.spec.js
+++ spec/api-client.spec.js
@@ -9,7 +9,9 @@ describe("ApiClient",function() {
   };
 
   var ajaxSpy;
+  var sendOptions = { cache: true };
 
+  var ajaxRequest = {};
   const baseUrl = "http://example.com";
   const streamsPath = "/streams/";
   const streamsUrl = baseUrl + streamsPath;
@@ -21,23 +23,44 @@ describe("ApiClient",function() {
     };
 
     client = new ApiClient(baseUrl,options);
-    ajaxSpy = spyOn(ajaxInterface,"ajax").and.callThrough();
-  });
 
-  it(".send() makes Ajax requests with data",function() {
-    var options = { method: "POST", cache: true };
-
-    client.send(streamsPath,data,options);
-
-    expect(ajaxSpy).toHaveBeenCalledWith(streamsUrl,{ data: data, method: "POST", cache: true });
+    ajaxSpy = spyOn(ajaxInterface,"ajax").
+      and.
+      returnValue(ajaxRequest);
   });
 
   it(".get() makes GET requests",function() {
-    var options = { cache: true };
-
-    client.get(streamsPath,data,options);
-
+    client.get(streamsPath,data,sendOptions);
     expect(ajaxSpy).toHaveBeenCalledWith(streamsUrl,{ data: data, method: "GET", cache: true });
+  });
+
+  it(".get() returns the request object",function() {
+    expect(client.get(streamsPath,data,sendOptions)).toBe(ajaxRequest);
+  });
+
+  it(".post() makes POST requests",function() {
+    client.post(streamsPath,data,sendOptions);
+    expect(ajaxSpy).toHaveBeenCalledWith(streamsUrl,{ data: data, method: "POST", cache: true });
+  });
+
+  it(".post() returns the request object",function() {
+    expect(client.post(streamsPath,data,sendOptions)).toBe(ajaxRequest);
+  });
+
+  it(".patch() makes PATCH requests",function() {
+    client.patch(streamsPath,data,sendOptions);
+    expect(ajaxSpy).toHaveBeenCalledWith(streamsUrl,{ data: data, method: "PATCH", cache: true });
+  });
+
+  it(".patch() returns the request object",function() {
+    expect(client.patch(streamsPath,data,sendOptions)).toBe(ajaxRequest);
+  });
+
+  it(".send() makes generic Ajax requests, with data",function() {
+    var customOptions = { method: "HEAD", cache: true };
+
+    client.send(streamsPath,data,customOptions);
+    expect(ajaxSpy).toHaveBeenCalledWith(streamsUrl,{ data: data, method: "HEAD", cache: true });
   });
 
   it("sets Ajax authorization header",function() {
