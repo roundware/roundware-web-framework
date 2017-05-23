1499ffa4ff9ff4a62c9b29d2b37e21024cb1afd8
Mike Subelsky <mike@subelsky.com>
2017-05-23 12:04:43 -0400

    Additional docs for User class

--- src/user.js
+++ src/user.js
@@ -8,7 +8,7 @@ var userName = "(anonymous)";
 export class User {
   /** Create a User
    * @param {Object} options - Various configuration parameters for this user
-   * @param {apiClient} options.name - the API client object to use for server API calls
+   * @param {apiClient} options.apiClient - the API client object to use for server API calls
    * @param {String} options.deviceId - this value distinguishes a particular user, who may be anonymous, to the server; by default we will fingerprint the browser to get this value, but you can supply your own value (useful if your app has a preexisting authorization scheme)
    * @param {String} [options.clientType = "web"] 
    **/
@@ -27,7 +27,8 @@ export class User {
   }
 
   /** Make an API call to associate the (possibly anonymous) application user with a Roundware user account.
-   * Upon success, this function receives an auth token, which is passed onto the apiClient object. **/
+   * Upon success, this function receives an auth token, which is passed onto the apiClient object.
+   * @returns {Promise} represents the pending API call **/
   connect() {
     var data = {
       device_id: deviceId,
