2a0045ae4c8c2a8163512953c18b19b3ba364d50
Mike Subelsky <mike@subelsky.com>
2017-06-28 16:41:43 -0400

    partial fix for noisy test logging

--- src/shims.js
+++ src/shims.js
@@ -1,19 +1,12 @@
-// by default, we assume no access to console, so we create a dummy logger
-let logger = {
-  error: () => {},
-  warn : () => {},
-  info : () => {},
-  log  : () => {}
-};
+import * as logger from 'loglevel';
 
-// If a console is available, and we are in a browser, always use it; if we're in NodeJS, only use the console if the ROUNDWARE_DEBUG environment variable is set to "true"
-// We use the existence of the global variable 'process' as a test for whether
-if ((typeof console !== 'undefined') || (typeof(process) !== 'undefined' && process.env.ROUNDWARE_DEBUG === "true")) {
+logger.disableAll();
+
+if ((typeof(process) !== 'undefined' && process.env.ROUNDWARE_DEBUG === "true")) {
   /* istanbul ignore next */
-  logger = console;
+  logger.setDefaultLevel('debug');
 }
 
 module.exports = {
   logger: logger
 };
-
