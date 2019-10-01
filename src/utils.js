3ebc614948ca6358cb20d4572d0672b712fad81c
Mike Subelsky <mike@subelsky.com>
2019-10-01 16:11:39 -0400

    add random() utils method

--- src/utils.js
+++ src/utils.js
@@ -12,3 +12,24 @@ export const isEmpty = array => !array || array.length < 1;
 
 // @see https://eslint.org/docs/rules/no-prototype-builtins
 export const hasOwnProperty = (target,propName) => Object.prototype.hasOwnProperty.call(target,propName);
+
+// @see https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_minby-and-_maxby
+//const makeSelect = (comparator) => (a,b) => comparator(a,b) ? a : b;
+//export const minValue = makeSelect((a,b) => a <= b);
+//const minByValue = makeSelect((a, b) => a.value <= b.value)
+//const maxByValue = makeSelect((a, b) => a.value >= b.value)
+
+// @see https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+export const random = (a = 1, b = 0) => {
+  const lower = Math.min(a,b);
+  const upper = Math.max(a,b);
+
+  return lower + Math.random() * (upper - lower);
+};
+
+export const randomInt = (a = 1, b = 0) => {
+  const lower = Math.ceil(Math.min(a,b));
+  const upper = Math.floor(Math.max(a,b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
