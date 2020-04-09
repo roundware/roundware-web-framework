b4ae141098567231113fb8663e60cf5f3e3cc586
Taylor Snead <taylorsnead@gmail.com>
2020-04-08 22:42:13 -0400

    :sparkles: Implement voting for assets

--- src/asset.js
+++ src/asset.js
@@ -1,8 +1,8 @@
 /* global process */
-const PATH = process.env.NODE_ENV === 'development' ? '/assets/?created__lte=2019-08-15T18:06:39' : '/assets/';
+export const PATH = process.env.NODE_ENV === 'development' ? '/assets/?created__lte=2019-08-15T18:06:39' : '/assets/';
 
 export class Asset {
-  constructor(projectId,{ apiClient }) {
+  constructor(projectId, { apiClient }) {
     this.projectId = projectId;
     this.apiClient = apiClient;
   }
@@ -13,6 +13,6 @@ export class Asset {
 
   connect(data = {}) {
     const options = { ...data, project_id: this.projectId };
-    return this.apiClient.get(PATH,options);
+    return this.apiClient.get(PATH, options);
   }
 }
