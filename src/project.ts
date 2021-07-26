bbd5c6aefaba6823099e79cb491b091f4db4aee3
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-07-26 17:53:30 +0530

    Initial TypeScript conversion

--- src/project.ts
+++ src/project.ts
@@ -1,24 +1,33 @@
 import { GeoListenMode } from "./mixer";
 
 export class Project {
-  constructor(newProjectId, { apiClient }) {
+  projectId: number;
+  projectName: string;
+  apiClient: Options[`apiClient`];
+  legalAgreement: unknown;
+  recordingRadius: unknown;
+  maxRecordingLength: unknown;
+  location: Coordinates = { latitude: 1, longitude: 1 };
+  mixParams: {};
+
+  constructor(newProjectId: number, { apiClient }: Pick<Options, `apiClient`>) {
     this.projectId = newProjectId;
     this.projectName = "(unknown)";
     this.apiClient = apiClient;
     this.mixParams = {};
   }
 
-  toString() {
+  toString(): string {
     return `Roundware Project '${this.projectName}' (#${this.projectId})`;
   }
 
-  async connect(sessionId) {
+  async connect(sessionId: string): Promise<string> {
     const path = "/projects/" + this.projectId + "/";
 
     const requestData = { session_id: sessionId };
 
     try {
-      const data = await this.apiClient.get(path, requestData);
+      const data:  = await this.apiClient.get(path, requestData);
       //console.info({ PROJECTDATA: data });
 
       this.projectName = data.name;
@@ -42,7 +51,7 @@ export class Project {
     }
   }
 
-  uiconfig(sessionId) {
+  async uiconfig(sessionId: string): Promise<ApiClientGet> {
     const path = "/projects/" + this.projectId + "/uiconfig/";
     const data = { session_id: sessionId };
 
