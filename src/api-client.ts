b06c2cd89d5f1b01d31f73078c46681e9e67a54d
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-07-26 12:11:57 +0530

    API Client Initial typescript migration

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,14 +1,32 @@
-const GENERIC_ERROR_MSG = 'We were unable to contact the audio server, please try again.';
+const GENERIC_ERROR_MSG =
+  "We were unable to contact the audio server, please try again.";
 
 // Handles HTTP interactions with the Roundware API server, v2.
 // NOTE: Every HTTP method except ".get()" will cause most browsers to issue a preflight requirements check to the server via the OPTIONS verb,
 // to verify CORS will allow the response to load in the browser. Sometimes this OPTIONS call can get obscured in debugging tools.
 // @see http://roundware.org/docs/terminology/index.html
-export class ApiClient {
+
+type ApiClientOptions = {
+  method?: string;
+  contentType?: string;
+};
+
+export interface IApiClient {
+  get(path: string, data: object, options: ApiClientOptions): Promise<object>;
+  post(path: string, data: object, options: ApiClientOptions): Promise<object>;
+  patch(path: string, data: object, options: ApiClientOptions): Promise<object>;
+  send(path: string, data: object, options: ApiClientOptions): Promise<object>;
+}
+export class ApiClient implements IApiClient {
   /** Create a new ApiClient
    * @param {String} baseServerUrl - identifies the Roundware server to receive API requests
    * @param {Boolean} [options.fetch = fetch] - for testing purposes, you can inject the fetch mechanism to use for making network requests **/
-  constructor(window,baseServerUrl) {
+
+  private _jQuery: any;
+  _serverUrl: string;
+  _authToken: string;
+
+  constructor(window: Window, baseServerUrl: string) {
     this._jQuery = window.jQuery;
     this._serverUrl = baseServerUrl;
     this._authToken = null;
@@ -18,28 +36,28 @@ export class ApiClient {
    * @param {String} path - the path for your API request, such as "/streams/"
    * @param {Object} options - see the "send" method
    * @see {send} **/
-  get(path,data,options = {}) {
+  async get(path: string, data: object, options: ApiClientOptions = {}) {
     options.method = "GET";
-    options.contentType = 'x-www-form-urlencoded';
-    return this.send(path,data,options);
+    options.contentType = "x-www-form-urlencoded";
+    return await this.send(path, data, options);
   }
 
   /** Make a POST request to the Roundware server
    * @param {String} path - the path for your API request, such as "/streams/"
    * @param {Object} options - see the "send" method
    * @see {send} **/
-  post(path,data,options = {}) {
+  async post(path: string, data: object, options: ApiClientOptions = {}) {
     options.method = "POST";
-    return this.send(path,data,options);
+    return await this.send(path, data, options);
   }
 
   /** Make a PATCH request to the Roundware server
    * @param {String} path - the path for your API request, such as "/streams/"
    * @param {Object} options - see the "send" method
    * @see {send} **/
-  patch(path,data,options = {}) {
+  async patch(path: string, data: object, options: ApiClientOptions = {}) {
     options.method = "PATCH";
-    return this.send(path,data,options);
+    return await this.send(path, data, options);
   }
 
   /** Transmit an Ajax request to the Roundware API. Note that the Roundware Server expects paths to end with a trailing slash: /sessions/ instead of /sessions
@@ -50,48 +68,55 @@ export class ApiClient {
    * @todo might be a good place to implement exponential retry of certain types of errors
    * @todo as of 2019, the fetch() polyfills are good enough that we should be able to get rid of JQuery dependency
    * **/
-  async send(path,data = {},{ contentType, method, ...urlOptions }) {
+  async send(
+    path: string,
+    data: object = {},
+    { contentType, method, ...urlOptions }: ApiClientOptions
+  ): Promise<object> {
     const url = new URL(this._serverUrl + path);
 
-    const requestInit = {
+    const requestInit: RequestInit = {
       method,
-      mode: 'cors',
+      mode: "cors",
+      body: "",
+      headers: {},
     };
-    
-    const queryParams = new URLSearchParams('');
+
+    const queryParams = new URLSearchParams("");
 
     for (let key in urlOptions) {
-      queryParams.append(key,urlOptions[key]);
+      queryParams.append(key, urlOptions[key]);
     }
 
     const headers = {};
 
     switch (method) {
       // for these cases, anything in 'data' has to be appended to query string
-      case 'GET':
-      case 'HEAD':
-        for (let key in data) queryParams.append(key,data[key]);
+      case "GET":
+      case "HEAD":
+        for (let key in data) queryParams.append(key, data[key]);
         break;
       // for other HTTP methods, 'data' has to be turned into a request body, with a properly-set Content-Type required by the Roundware server API.
       default:
+        let stringData: string;
         if (!contentType) {
           // If you don't specify a contentType, we assume you want us to convert your payload to JSON
-          contentType = 'application/json';
-          data = JSON.stringify(data);
+          contentType = "application/json";
+          stringData = JSON.stringify(data);
         }
 
-      if (contentType != "multipart/form-data") {
-        headers['Content-Type'] = contentType;
-      }
-        requestInit.body = data;
+        if (contentType != "multipart/form-data") {
+          headers["Content-Type"] = contentType;
+        }
+        requestInit.body = stringData;
 
         break;
     }
 
-    url.search = queryParams;
+    url.search = queryParams.toString();
 
     if (this._authToken) {
-      headers['Authorization'] = this._authToken;
+      headers["Authorization"] = this._authToken;
     }
 
     requestInit.headers = headers;
@@ -99,14 +124,14 @@ export class ApiClient {
     let response;
 
     try {
-      response = await fetch(url,requestInit);
+      response = await fetch(url.toString(), requestInit);
     } catch (error) {
-      console.error('Roundware network error:',error.message);
+      console.error("Roundware network error:", error.message);
       throw GENERIC_ERROR_MSG;
     }
 
     if (!response.ok) {
-      console.error('Roundware API error, code:',response.status);
+      console.error("Roundware API error, code:", response.status);
       throw GENERIC_ERROR_MSG;
     }
 
@@ -114,14 +139,14 @@ export class ApiClient {
       const json = await response.json();
       return json;
     } catch (error) {
-      console.error('Unable to decode Roundware response',error);
+      console.error("Unable to decode Roundware response", error);
       throw GENERIC_ERROR_MSG;
     }
   }
 
   /** Set the authorization token to use as the header for future API requests. Most Roundware API calls require an auth token to be set.
    * @param {String} authToken - characters to use in the authorization header **/
-  set authToken(tokenStr) {
+  set authToken(tokenStr: string) {
     this._authToken = `token ${tokenStr}`;
   }
 }
