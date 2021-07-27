9ce65f0f241fa8a8c7ebfea9f12784abf2735016
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-07-27 11:41:27 +0530

    types for asset and mix params

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,15 @@
-type Coordinates = {
+import { Point } from "@turf/helpers";
+export type Coordinates = {
   latitude: number;
   longitude: number;
+};
+
+export type Asset = {
+  weight: number;
+  locationPoint?: Coordinates;
+  listenerPoint?: Point;
+};
+
+export type MixParams = {
+  ordering?: string;
 };
