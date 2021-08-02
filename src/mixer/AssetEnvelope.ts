a36e28cf412c2604c55abe7b73b02a380d08fd54
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-02 11:32:51 +0530

    Removed IAssetEvelope

--- src/mixer/AssetEnvelope.ts
+++ src/mixer/AssetEnvelope.ts
@@ -1,9 +1,8 @@
 import { IAssetData } from "../types";
-import { IAssetEnvelope } from "../types/mixer/AssetEnvelope";
 import { ITrackOptions } from "../types/mixer/TrackOptions";
 import { random } from "../utils";
 
-export class AssetEnvelope implements IAssetEnvelope {
+export class AssetEnvelope {
   asset: IAssetData;
   assetId: string | number | undefined;
   minDuration: number;
