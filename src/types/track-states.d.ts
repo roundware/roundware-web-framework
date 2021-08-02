8b71cf494f7082f9524632835c758c474efa62b5
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-02 11:34:31 +0530

    Use single common interface

--- src/types/track-states.d.ts
+++ src/types/track-states.d.ts
@@ -1,72 +1,36 @@
 import { IMixParams } from ".";
+import { PlaylistAudiotrack } from "../playlistAudioTrack";
+import {
+  DeadAirState,
+  FadingInState,
+  FadingOutState,
+  LoadingState,
+  PlayingState,
+  TimedTrackState,
+  WaitingForAssetState,
+} from "../TrackStates";
 
 interface ICommonStateProperties {
-  play: () => void;
-  pause(): void;
-  finish(): void;
-  skip(): void;
-  replay(): void;
-  updateParams(mixParams: IMixParams): void;
-  toString(): string;
-}
-export interface ILoadingState extends ICommonStateProperties {}
-
-export interface ITimedTrackState extends ICommonStateProperties {
+  track: PlaylistAudiotrack;
+  timerApproximateEndingAtMs?: number;
   play(nextStateSecs?: number): number | void;
   pause(): void;
-  clearTimer(): any;
   finish(): void;
-  setNextStateTimer(timeMs?: number): void;
-  setNextState(): void;
   skip(): void;
   replay(): void;
-  setLoadingState(): void;
   updateParams(mixParams: IMixParams): void;
-}
-
-export interface IDeadAirState extends ICommonStateProperties {
-  play(): void;
-  setNextState(): void;
   toString(): string;
-  updateParams(mixParams: IMixParams): void;
-}
-
-export interface IFadingInState extends ICommonStateProperties {
-  play(): void;
-  pause(): void;
-  setNextState(): void;
-  toString(): string;
-  updateParams(mixParams: IMixParams): void;
-}
-
-export interface IPlayingState extends ICommonStateProperties {
-  play(): void;
-  pause(): void;
-  toString(): string;
-  setNextState(): void;
-  updateParams(mixParams: IMixParams): void;
-}
-
-export interface IFadingOutState extends ICommonStateProperties {
-  play(): void;
-  pause(): void;
-  setNextState(): void;
-  toString(): string;
-  updateParams(mixParams: IMixParams): void;
-}
-
-export interface IWaitingForAssetState extends ICommonStateProperties {
-  play(): void;
-  updateParams(mixParams: IMixParams): void;
-  setNextState(): void;
-  toString(): string;
+  clearTimer?(): any;
+  setNextStateTimer?(timeMs?: number): void;
+  setNextState?(): void;
+  setLoadingState?(): void;
 }
 
 export type ITrackStates =
-  | IPlayingState
-  | ILoadingState
-  | IWaitingForAssetState
-  | IFadingInState
-  | IFadingOutState
-  | ITimedTrackState
-  | IDeadAirState;
+  | PlayingState
+  | LoadingState
+  | WaitingForAssetState
+  | FadingInState
+  | FadingOutState
+  | TimedTrackState
+  | DeadAirState;
