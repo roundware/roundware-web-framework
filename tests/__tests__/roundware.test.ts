6d23e0673e4a1332c49e55ba10c0a5ffc63a3b8a
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-17 20:37:30 +0530

    throw error if options not passed

--- tests/__tests__/roundware.test.ts
+++ tests/__tests__/roundware.test.ts
@@ -4,7 +4,22 @@ describe("Roundware", () => {
   it("throw error if windowScope not passed", () => {
     // @ts-expect-error
     expect(() => new Roundware()).toThrow(
-      new MissingArgumentError(`windowScope`, `instantiating Roundware`)
+      new MissingArgumentError(
+        `windowScope`,
+        `instantiating Roundware`,
+        `window`
+      )
+    );
+  });
+
+  it("throws error if options not passed", () => {
+    // @ts-expect-error
+    expect(() => new Roundware(global.window)).toThrow(
+      new MissingArgumentError(
+        `options`,
+        `instantiating Roundware`,
+        `IRoundwareConstructorOptions`
+      )
     );
   });
 });
