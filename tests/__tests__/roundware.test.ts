27eda2a29e35ca660ea4f8b21464ad1af4a38cbf
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-17 21:43:20 +0530

    wrap with describe for instantiation

--- tests/__tests__/roundware.test.ts
+++ tests/__tests__/roundware.test.ts
@@ -1,3 +1,4 @@
+import { noAssetData } from "../../src/constants/warning";
 import {
   InvalidArgumentError,
   MissingArgumentError,
@@ -5,56 +6,75 @@ import {
 import { GeoListenMode, Roundware } from "../../src/roundware";
 import { IRoundwareConstructorOptions } from "../../src/types/roundware";
 describe("Roundware", () => {
-  it("throw error if windowScope not passed", () => {
-    // @ts-expect-error
-    expect(() => new Roundware()).toThrow(
-      new MissingArgumentError(
-        `windowScope`,
-        `instantiating Roundware`,
-        `window`
-      )
-    );
+  describe("Instantiation", () => {
+    it("throw error if windowScope not passed", () => {
+      // @ts-expect-error
+      expect(() => new Roundware()).toThrow(
+        new MissingArgumentError(
+          `windowScope`,
+          `instantiating Roundware`,
+          `window`
+        )
+      );
+    });
+
+    it("throws error if options not passed", () => {
+      // @ts-expect-error
+      expect(() => new Roundware(global.window)).toThrow(
+        new MissingArgumentError(
+          `options`,
+          `instantiating Roundware`,
+          `IRoundwareConstructorOptions`
+        )
+      );
+    });
+
+    it("throw error if serverUrl is not passed or invalid", () => {
+      // @ts-expect-error
+      expect(() => new Roundware(global.window, {})).toThrow(
+        new InvalidArgumentError(
+          `options.serverUrl`,
+          `string`,
+          `instantiating Roundware`
+        )
+      );
+    });
+
+    it("throw error if projectId is not passed or invalid", () => {
+      expect(
+        () =>
+          // @ts-expect-error
+          new Roundware(global.window, {
+            serverUrl: "mock.roundware.com",
+          })
+      ).toThrow(
+        new InvalidArgumentError(
+          `options.serverUrl`,
+          `string`,
+          `instantiating Roundware`
+        )
+      );
+    });
+
+    it("creates a Roundware instance successfully", () => {
+      const options: IRoundwareConstructorOptions = {
+        serverUrl: "https://prod.roundware.com/api/2",
+        projectId: 10,
+        assetFilters: {},
+        listenerLocation: {
+          latitude: 50,
+          longitude: 155,
+        },
+        deviceId: "",
+        apiClient: undefined,
+        geoListenMode: GeoListenMode.DISABLED,
+      };
+      const roundware = new Roundware(global.window, options);
+      expect(roundware).toBeInstanceOf(Roundware);
+    });
   });
 
-  it("throws error if options not passed", () => {
-    // @ts-expect-error
-    expect(() => new Roundware(global.window)).toThrow(
-      new MissingArgumentError(
-        `options`,
-        `instantiating Roundware`,
-        `IRoundwareConstructorOptions`
-      )
-    );
-  });
-
-  it("throw error if serverUrl is not passed or invalid", () => {
-    // @ts-expect-error
-    expect(() => new Roundware(global.window, {})).toThrow(
-      new InvalidArgumentError(
-        `options.serverUrl`,
-        `string`,
-        `instantiating Roundware`
-      )
-    );
-  });
-
-  it("throw error if projectId is not passed or invalid", () => {
-    expect(
-      () =>
-        // @ts-expect-error
-        new Roundware(global.window, {
-          serverUrl: "mock.roundware.com",
-        })
-    ).toThrow(
-      new InvalidArgumentError(
-        `options.serverUrl`,
-        `string`,
-        `instantiating Roundware`
-      )
-    );
-  });
-
-  it("creates a Roundware instance successfully", () => {
+  describe("Access Properties", () => {
     const options: IRoundwareConstructorOptions = {
       serverUrl: "https://prod.roundware.com/api/2",
       projectId: 10,
@@ -68,6 +88,17 @@ describe("Roundware", () => {
       geoListenMode: GeoListenMode.DISABLED,
     };
     const roundware = new Roundware(global.window, options);
-    expect(roundware).toBeInstanceOf(Roundware);
+    it("Return array of assets", () => {
+      const assets = roundware.assets();
+      console.log(assets);
+      expect(Array.isArray(assets)).toBe(true);
+    });
+
+    it(".connect() - return Promise<{uiConfig: IUiConfig}>", async () => {
+      const data = await roundware.connect();
+      console.log(data);
+      expect(data).toHaveProperty("uiConfig");
+      expect(data.uiConfig).toHaveProperty("speak");
+    });
   });
 });
