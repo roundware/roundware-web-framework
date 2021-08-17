5bb36e25a86d9a8fdea6b3a9635426f9dcb1c381
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-17 21:08:03 +0530

    roundware instantiation tests

--- tests/__tests__/roundware.test.ts
+++ tests/__tests__/roundware.test.ts
@@ -1,5 +1,9 @@
-import { MissingArgumentError } from "../../src/errors/app.errors";
-import { Roundware } from "../../src/roundware";
+import {
+  InvalidArgumentError,
+  MissingArgumentError,
+} from "../../src/errors/app.errors";
+import { GeoListenMode, Roundware } from "../../src/roundware";
+import { IRoundwareConstructorOptions } from "../../src/types/roundware";
 describe("Roundware", () => {
   it("throw error if windowScope not passed", () => {
     // @ts-expect-error
@@ -21,5 +25,49 @@ describe("Roundware", () => {
         `IRoundwareConstructorOptions`
       )
     );
+  });
+
+  it("throw error if serverUrl is not passed or invalid", () => {
+    // @ts-expect-error
+    expect(() => new Roundware(global.window, {})).toThrow(
+      new InvalidArgumentError(
+        `options.serverUrl`,
+        `string`,
+        `instantiating Roundware`
+      )
+    );
+  });
+
+  it("throw error if projectId is not passed or invalid", () => {
+    expect(
+      () =>
+        // @ts-expect-error
+        new Roundware(global.window, {
+          serverUrl: "mock.roundware.com",
+        })
+    ).toThrow(
+      new InvalidArgumentError(
+        `options.serverUrl`,
+        `string`,
+        `instantiating Roundware`
+      )
+    );
+  });
+
+  it("creates a Roundware instance successfully", () => {
+    const options: IRoundwareConstructorOptions = {
+      serverUrl: "https://prod.roundware.com/api/2",
+      projectId: 10,
+      assetFilters: {},
+      listenerLocation: {
+        latitude: 50,
+        longitude: 155,
+      },
+      deviceId: "",
+      apiClient: undefined,
+      geoListenMode: GeoListenMode.DISABLED,
+    };
+    const roundware = new Roundware(global.window, options);
+    expect(roundware).toBeInstanceOf(Roundware);
   });
 });
