1357e1ebbfaf88ab8aa61f2dfb7b8abb16910b6d
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-09-12 15:03:21 +0530

    web audio stereo panner

--- src/audioPanner.ts
+++ src/audioPanner.ts
@@ -1,4 +1,5 @@
-import { Howl } from "howler";
+import { IAudioContext, IStereoPannerNode } from "standardized-audio-context";
+import { random } from "./utils";
 
 export class AudioPanner {
   minpanpos: number;
@@ -12,74 +13,42 @@ export class AudioPanner {
   currentPosition?: number;
   panningTowards?: string;
   timerId?: NodeJS.Timeout;
-  sound?: Howl;
-
+  panNode: IStereoPannerNode<IAudioContext>;
+  audioContext: IAudioContext;
   constructor(
     minpanpos: number = 0,
     maxpanpos: number = 0,
     minpanduration: number = 0,
-    maxpanduration: number = 0
+    maxpanduration: number = 0,
+    panNode: IStereoPannerNode<IAudioContext>,
+    audioContext: IAudioContext
   ) {
-    this.minpanpos = minpanpos;
-    this.maxpanpos = maxpanpos;
-    this.minpanduration = minpanduration;
-    this.maxpanduration = maxpanduration;
+    this.minpanpos = -1;
+    this.maxpanpos = 1;
+    this.minpanduration = 1;
+    this.maxpanduration = 3;
+    this.panNode = panNode;
+    this.audioContext = audioContext;
     this.updateParams();
   }
 
-  updateAudioInstance(howl: Howl) {
-    this.clear();
-    this.sound = howl;
-  }
-
-  calculatePerSecondChange() {
-    this.panPositionChangePerSecond =
-      (this.finalPosition! - this.initialPosition!) / this.duration!;
-  }
   updateParams() {
     // when it's first time use random value for initial else use final
-    if (this.finalPosition === undefined) {
-      this.initialPosition = randomFromRange(this.minpanpos, this.maxpanpos);
-    } else this.initialPosition = this.finalPosition;
-
-    this.finalPosition = randomFromRange(this.minpanpos, this.maxpanpos);
-    this.duration = randomFromRange(this.minpanduration, this.maxpanduration);
+    this.finalPosition = random(this.minpanpos, this.maxpanpos);
+    this.duration = random(this.minpanduration, this.maxpanduration);
     this.currentPosition = this.initialPosition;
-
-    this.calculatePerSecondChange();
-
-    function randomFromRange(min: number, max: number) {
-      return Math.random() * (max - min) + min;
-    }
   }
 
   start() {
-    if (this.initialPosition! > this.finalPosition!)
-      this.panningTowards = "left";
-    else this.panningTowards = "right";
-
+    this.panNode.pan.linearRampToValueAtTime(
+      this.finalPosition!,
+      this.audioContext.currentTime + this.duration!
+    );
     //  create a timeout loop function
     this.timerId = setTimeout(() => {
-      this.sound!.stereo(this.currentPosition!);
-      if (!this.sound?.playing()) return this.start();
-
-      let isCompletelyPanned;
-      // toward left means final position is lesser than initial
-      // toward right means final position is greater than intiial
-      if (this.panningTowards === `left`) {
-        isCompletelyPanned = this.currentPosition! <= this.finalPosition!;
-      } else isCompletelyPanned = this.currentPosition! >= this.finalPosition!;
-
-      if (!isCompletelyPanned) {
-        this.currentPosition! += this.panPositionChangePerSecond!;
-        // keep ramping until it reaches final position
-        this.start();
-      } else {
-        // now make current as initial and new random as final
-        this.updateParams();
-        this.start();
-      }
-    }, 1000);
+      this.updateParams();
+      this.start();
+    }, this.duration! * 1000);
   }
 
   clear() {
