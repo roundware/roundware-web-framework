7fd33217ecb741abe3e0546662ce983914f3edfb
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-03-02 20:26:20 +0530

    event types

---
+++ src/types/events.d.ts
@@ -0,0 +1,21 @@
+export type EventType =
+  | "start_session"
+  | "play_stream"
+  | "pause_stream"
+  | "filter_stream"
+  | "location_update"
+  | "change_listen_mode"
+  | "start_record"
+  | "end_record"
+  | "upload_asset"
+  | "share_map";
+
+export type EventPayload = {
+  latitude?: number;
+  longitude?: number;
+  tag_ids?: string[];
+  session_id?: string;
+  event_type?: EventType;
+  client_time?: string;
+  data?: string;
+};
