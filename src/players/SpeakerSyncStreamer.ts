7c2469c38b02b50a1e090cfcb9e887583492ae05
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-04-14 20:26:30 +0530

    change playbackRate to sync

--- src/players/SpeakerSyncStreamer.ts
+++ src/players/SpeakerSyncStreamer.ts
@@ -8,7 +8,6 @@ import { SpeakerConfig } from "../types/roundware";
 import { ISpeakerPlayer, SpeakerConstructor } from "../types/speaker";
 import { cleanAudioURL, makeAudioSafeToPlay, speakerLog } from "../utils";
 
-export type SpeakerState = "playing" | "waiting";
 export class SpeakerSyncStreamer implements ISpeakerPlayer {
   isSafeToPlay: boolean = true;
   playing: boolean = false;
@@ -21,7 +20,7 @@ export class SpeakerSyncStreamer implements ISpeakerPlayer {
   gainNode: IGainNode<IAudioContext>;
   mediaSource: IMediaElementAudioSourceNode<IAudioContext>;
   context: IAudioContext;
-  state: SpeakerState;
+
   constructor({ audioContext, config, uri, id }: SpeakerConstructor) {
     this.id = id;
     this.config = config;
@@ -33,13 +32,13 @@ export class SpeakerSyncStreamer implements ISpeakerPlayer {
     this.audio.loop = config.loop || false;
     this.gainNode = audioContext.createGain();
     this.gainNode.gain.value = 0;
+    this.audio.crossOrigin = "anonymous";
+    this.audio.loop = false;
     this.mediaSource = audioContext.createMediaElementSource(this.audio);
     this.mediaSource.connect(this.gainNode).connect(audioContext.destination);
     this.audio.preload = "auto";
-    this.state = "waiting";
+
     const that = this;
-    this.audio.addEventListener("playing", () => that.setState("playing"));
-    this.audio.addEventListener("waiting", () => that.setState("waiting"));
 
     this.log(`sync streamer initiaed`);
   }
@@ -47,56 +46,64 @@ export class SpeakerSyncStreamer implements ISpeakerPlayer {
   started = false;
 
   async play(): Promise<boolean> {
-    if (this.playing) return true;
-    if (this.started) {
-      // set gain to last paused
-      this.gainNode.connect(this.context.destination);
-      this.gainNode.gain.value = 1;
-      this.playing = true;
-      this.log(`Playing... ${this.audio.currentTime}`);
-      return true;
-    }
     try {
       await this.audio.play();
-      this.log(`Started...`);
-      this.started = true;
-      return true;
+      this.playing = true;
+      this.log(`Playing...`);
     } catch (e) {
-      console.error(`Failed to play`, e);
-      return false;
+      console.error(e);
+      this.playing = false;
     }
+    return true;
   }
   pause(): void {
-    this.playing = false;
-    this.gainNode.disconnect();
+    this.audio.pause();
   }
   replay(): void {
     this.audio.currentTime = 0;
   }
+
+  startedAt: Date | null = null;
   timerStart(): void {
+    this.startedAt = new Date();
+    this.syncTracker = Number(
+      setInterval(() => {
+        this.trackSync();
+      }, this.config.syncCheckInterval || 2500)
+    );
     this.play();
   }
   timerStop(): void {
     this.pause();
+    clearInterval(this.syncTracker);
   }
   fadingDestination = 0;
   fading = false;
   fade(destinationVolume?: number, duration: number = 3): void {
-    // if (this.fadingDestination == destinationVolume && this.fading) {
-    //   return;
-    // }
-    // if (typeof destinationVolume == "number") {
-    //   this.fadingDestination = destinationVolume;
-    // }
+    if (
+      typeof destinationVolume != "number" ||
+      (destinationVolume == this.fadingDestination && this.fading)
+    )
+      return;
+    this.fadingDestination = destinationVolume;
 
-    // this.gainNode.gain.linearRampToValueAtTime(
-    //   this.fadingDestination,
-    //   this.context.currentTime + duration
-    // );
-    // setTimeout(() => {
-    //   this.fading = false;
-    // }, 3000);
-    this.gainNode.gain.value = 1;
+    if (this.gainNode.gain.value < 0.05 && destinationVolume > 0.05) {
+      this.log(`Connected ${this.gainNode.numberOfOutputs}`);
+      this.gainNode.connect(this.context.destination);
+    }
+    this.fading = true;
+    this.gainNode.gain.linearRampToValueAtTime(
+      destinationVolume,
+      this.context.currentTime + duration
+    );
+    const that = this;
+    setTimeout(() => {
+      if (that.gainNode.gain.value < 0.05) {
+        that.gainNode.disconnect();
+        that.log(`Disconnected`);
+      }
+      that.fading = false;
+    }, duration * 1000);
   }
   fadeOutAndPause(): void {
     this.fade(0);
@@ -106,16 +113,35 @@ export class SpeakerSyncStreamer implements ISpeakerPlayer {
   }
   onLoadingProgress(callback: (newPercent: number) => void): void {}
   onEnd(callback: () => void): void {}
+
   updateTime(newTime: number): void {
     this.audio.currentTime = newTime;
   }
 
-  stateCallback: (newState: SpeakerState) => void = () => {};
-  onStateUpdate(callback: (newState: SpeakerState) => any) {
-    this.stateCallback = callback;
-  }
-  setState(newState: "playing" | "waiting") {
-    this.state = newState;
-    this.stateCallback(this.state);
+  syncTracker?: number;
+  trackSync() {
+    if (!(this.startedAt instanceof Date)) return;
+    if (!this.playing) return;
+
+    const currentTime = new Date().getTime() - this.startedAt.getTime();
+    if (currentTime > this.audio.duration * 1000) return;
+
+    const audioTime = this.audio.currentTime * 1000;
+
+    const difference = currentTime - audioTime;
+
+    this.log(`Difference: ${difference} ms`);
+    if (Math.abs(difference) < (this.config.acceptableDelayMs || 50)) {
+      // within acceptable range;
+      this.audio.playbackRate = 1;
+    } else if (Math.abs(difference) > 1000) {
+      // difference is too much; try to seek instead;
+      this.audio.currentTime = currentTime / 1000;
+      this.audio.playbackRate = 1;
+    } else {
+      this.audio.playbackRate =
+        1 + difference / (this.config.syncCheckInterval || 2500);
+      this.log(`Changing Playback rate to ${this.audio.playbackRate}`);
+    }
   }
 }
